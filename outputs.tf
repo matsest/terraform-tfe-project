f56d56766d4fd1b116b2af303d69af27cd2bbf99
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2020-05-31 01:28:43 +0200

    Add `organization` output value

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "name_prefix" {
   description = "Echoes back the `name_prefix` input variable value, for convenience if passing the result of this module elsewhere as an object."
 }
 
+output "organization" {
+  value       = var.organization
+  description = "Echoes back the `organization` input variable value, for convenience if passing the result of this module elsewhere as an object."
+}
+
 output "environments" {
   value       = var.environments
   description = "Echoes back the `environments` input variable value, for convenience if passing the result of this module elsewhere as an object."
